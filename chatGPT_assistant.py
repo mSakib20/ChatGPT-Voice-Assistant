9836aec617e6b61ae2c68ad8959bf0d9ed0fb4b8
mSakib20 <msakib2022@fau.edu>
2023-07-04 23:41:08 -0400

    Impleneting code

---
+++ chatGPT_assistant.py
@@ -0,0 +1,15 @@
+# Importing the libraries for speech recognition, pyttsx3, and openAI
+import speech_recognition as sr
+import pyttsx3
+import openai
+
+# Setting up the openAI API key
+openai.api_key = "USE YOUR API"
+
+# Setting up the speech recognition engine
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)  # voices[1] = male voice
+
+recognizer = sr.Recognizer()
+microphone = sr.Microphone(device_index=1)
