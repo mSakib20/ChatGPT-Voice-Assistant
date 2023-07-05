98fd5cc576fe6626ad3caa9c5362e37f06a92d71
mSakib20 <msakib2022@fau.edu>
2023-07-05 00:04:51 -0400

    Setting up the conversation loop

--- chatGPT_assistant.py
+++ chatGPT_assistant.py
@@ -18,3 +18,19 @@ microphone = sr.Microphone(device_index=1)
 conversation = ""
 userName = "User"
 aiName = "chatGPT"
+
+# Setting up the conversation loop
+while True:
+    with microphone as source:
+        print("Say something!")
+        recognizer.adjust_for_ambient_noise(source, duration=0.2)
+        audio = recognizer.listen(source)
+        print("Got it!")
+    print("Processing...")
+
+    # Converting the audio to text
+    try:
+        userInput = recognizer.recognize_google(audio)
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+        continue
